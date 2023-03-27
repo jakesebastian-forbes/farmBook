f688306464926fb5b7bf066fee2b269efbd64371
jakesebastian-forbes <jforbes557@gmail.com>
2023-03-28 02:57:25 +0800

    added phpmailer with otp, weather ui

--- js/test_weather_api.js
+++ js/test_weather_api.js
@@ -1,30 +1,101 @@
 
+// ** STEPS 
+// ASK USER FOR LOCATION
+    // either by typing(city) or geolocation API 
+//
+//
+//
 
-// https://openweathermap.org/api/geocoding-api //! geocoding do this first
 
-// fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={66c41129d9056b9aa1b13ec1f2d20583}")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-
-//   https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+//   $.get("http://api.openweathermap.org/geo/1.0/direct?q=Nasugbu,PH&limit=1&appid=e2630aae7335274da681acc3f37f2620", function(data, status){
+//     alert("Data: " + data + "\nStatus: " + status);
+//   });
 
 // $.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={66c41129d9056b9aa1b13ec1f2d20583}", function(data, status){
 //     alert("Data: " + data + "\nStatus: " + status);
 //   });
 
+//   https://api.openweathermap.org/data/2.5/weather?lat=14.0745886&lon=120.626089&appid=e2630aae7335274da681acc3f37f2620
 
-//   $.get("http://api.openweathermap.org/geo/1.0/direct?q={Nasugbu},{PH}&limit={limit}&appid={API key}", function(data, status){
-//     alert("Data: " + data + "\nStatus: " + status);
-//   });
-
-  $("button").click(function(){
-    $.get("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e2630aae7335274da681acc3f37f2620", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-      });
-  });
-
-//   https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
+var api_data;
+var loc_lat,loc_long;
+var area = "Manila";
+var api_key = "e2630aae7335274da681acc3f37f2620"
+var request_link = "https://api.openweathermap.org/data/2.5/weather?q="+area+",PH&APPID=" +api_key;
+var permission_status = $("#permission_status");
+var text_location;
 
 
-// ! PROBLEM ! 
-// ! REQUEST NOT PASSING THROUGH BECAUSE OF LOCALHOST
+
+  
+
+
+function get_weather(){
+  // $.get(request_link, function(data, status){
+  //   api_data = data;  
+  //   area = api_data.name;
+  // alert("Data: " + data + "\nStatus: " + status +"\n Location : " + area);
+ 
+  // refresh_text();
+
+  // });
+
+  
+$.ajax({
+  url: request_link,
+  type: "GET",
+  async: false,
+  success: function(data, status){
+  api_data = data;  
+  // alert("Data: " + data + "\nStatus: " + status +"\n Location : " + area +","+api_data.name);
+  refresh_text();
+
+  }
+  })  
+
+}
+
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+      permission_status.innerHTML = "The Browser Does not Support Geolocation";
+    }
+  }
+
+  function showPosition(position) {
+      loc_lat = position.coords.latitude;
+      loc_long = position.coords.longitude;
+      request_link ="https://api.openweathermap.org/data/2.5/weather?lat="+loc_lat+"&lon="+loc_long+"&appid=" + api_key;
+      get_weather();
+  
+  }
+
+  function showError(error) {
+    if(error.PERMISSION_DENIED){
+        permission_status.innerHTML = "The User have denied the request for Geolocation.";
+      }
+  }
+
+  function refresh_text(){
+    $("#data")[0].textContent = JSON.stringify(api_data);
+    $("#location")[0].textContent = api_data.name;
+    
+  }
+
+
+get_weather();
+
+
+$("#btn_getLocation").click(function(){
+  getLocation();
+});
+
+
+$("#btn_txtloc").click(function(){ //! doesn't work just yet
+  text_location = document.getElementById("txt_location").value;
+  request_link = "https://api.openweathermap.org/data/2.5/weather?q="+text_location+",PH&APPID=" +api_key;
+  console.log(area);
+  get_weather();
+});
---
+++ php_func/php_mailer.php
@@ -0,0 +1,57 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '..\dependencies\phpmailer\vendor\autoload.php';
+require 'generate_otp.php';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 3;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp-relay.sendinblue.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'farmbook.ph.co@gmail.com';                     //SMTP username
+    $mail->Password   = 'j0zTn6f3PX4bGVyY';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('farmbook.ph.co@gmail.com', 'Farmbook');
+
+//https://app.sendinblue.com/settings/keys/smtp
+    $mail->addAddress('aybeniichan@gmail.com', 'Reignoel Rodriguez');     //Add a recipient
+
+
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'OTP code for account confirmation';
+    $mail->Body    = 'Hi aybeniichan! Your OTP code is :<b>'.generateNumericOTP(7).'</b>.
+    Please do not share this code to anyone. This code will expire in 10 minutes. oyy miss';
+    // $mail->AltBody = '';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
